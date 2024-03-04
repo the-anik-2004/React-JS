5bdcae27823e528b69c0cc1e950c4d2b1c9b6ce9
Anik Pal <130134332+the-anik-2004@users.noreply.github.com>
2024-03-04 11:06:02 +0530

    implement a button function to reduce code redudency

--- 05bg-color-changer/src/App.jsx
+++ 05bg-color-changer/src/App.jsx
@@ -4,34 +4,36 @@ import { useState } from 'react'
 function App() {
   const [color, setColor] = useState('#212121');
 
+  function Button({btnText}){
+    return(
+      <button onClick={()=>setColor(btnText)} className='rounded-full m-1' style={{backgroundColor:btnText}}>{btnText.toUpperCase()}</button>
+    )
+  }
+
   return (
     <div className='w-screen h-screen duration-900'
     style={{backgroundColor: color}}
     >
       <div className='flex flex-wrap fixed justify-center
        inset-x-4 px-2  bottom-12 rounded-full border-2 border-black' style={{backgroundColor:"aliceblue",color:"#fff"}}>
+
+        <Button btnText='red'/>
+        <Button btnText='green'/>
+        <Button btnText='blue'/>
+        <Button btnText='olive'/>
+        <Button btnText='yellow' className="text-black"/>
+        <Button btnText='pink'/>
+        <Button btnText='purple'/>
+        <Button btnText='lavender' className="text-gray-900"/>
+        <Button btnText='black'/>
         
-        <button onClick={()=>{setColor('red')}} className='m-1 rounded-full flex flex-wrap bg-red-500 ' >RED</button>
-        <button onClick={()=>{setColor('green')}} className='m-1 rounded-full flex flex-wrap bg-green-500 ' >GREEN</button>
-        <button onClick={()=>{setColor('blue')}} className='m-1 rounded-full flex flex-wrap bg-blue-500 ' >BLUE</button>
-        <button onClick={()=>{setColor('yellow')}} className='m-1 rounded-full flex flex-wrap bg-yellow-500 ' >YELLOW</button>
-        <button onClick={()=>{setColor('gray')}} className='m-1 rounded-full flex flex-wrap bg-gray-500 ' >GRAY</button>
-        <button onClick={()=>{setColor('pink')}} className='m-1 rounded-full flex flex-wrap bg-pink-500 ' >PINK</button>
-        <button onClick={()=>{setColor('violet')}} className='m-1 rounded-full flex flex-wrap bg-violet-500 ' >VIOLET</button>
-        <button onClick={()=>{setColor('white')}} className='m-1 rounded-full flex flex-wrap bg-white text-black border-2 border-black' >WHITE</button>
-        <button onClick={()=>{setColor('black')}} className='m-1 rounded-full flex flex-wrap bg-black-500 ' >BLACK</button>
-        <button onClick={()=>{setColor('orange')}} className='m-1 rounded-full flex flex-wrap bg-orange-500 ' >ORANGE</button>
-        <button onClick={()=>{setColor('cyan')}} className='m-1 rounded-full flex flex-wrap bg-cyan-500 ' >CYAN</button>
+       
       </div>
     </div>
   )
 }
 
-function Button(color){
-  return(
-    <button onClick={()=>setColor(color)} className='m-1 rounded-full flex flex-wrap ' style={{backgroundColor:color}} >{color.toUpperCase()}</button>
-  )
-}
+
 
 
 export default App
